728aaa13126cb19d10294548ba6ab98b7bf2dfa7
Toomcis <tomas.hnilicky1@gmail.com>
2025-06-19 19:54:15 +0200

    Added a Area2D, needs changing, made players functional fully, needs to fix trail

--- GameScene/Debug Scene/MainDebug.gd
+++ GameScene/Debug Scene/MainDebug.gd
@@ -1,10 +1,12 @@
 extends Control
 
-@onready var IPLabel = $RichTextLabel
+@onready var MenuLabel = $Nodes/Label
+@onready var DPI = (get_viewport().size.x + get_viewport().size.y)
 
-var multiplayer_peer = ENetMultiplayerPeer.new()
 const PORT = 7183
+var multiplayer_peer = ENetMultiplayerPeer.new()
 var ListOfPlayers = [0]
+var ServerStarted = false
 
 func _ready():
 	
@@ -13,45 +15,70 @@ func _ready():
 		print("Error starting Game server: ", err)
 		return
 	
-	# Assign the multiplayer peer to the scene
 	multiplayer.multiplayer_peer = multiplayer_peer
 
 	# Connect to multiplayer signals
 	multiplayer.peer_connected.connect(_on_peer_connected)
 	multiplayer.peer_disconnected.connect(_on_peer_disconnected)
+	ServerStarted = true
 
-	print("Server listening on port %d" % PORT)
-	
-	IPLabel.text = "IP to Join: " + str(IP.get_local_addresses()[3])# Add IP to connect instead of number of players
 
 func _process(delta):
 	pass
 
 func _on_peer_connected(id):
+	var CurrentPlayerIndex = ListOfPlayers.size()
 	print("Peer connected: %s" % id)
 	ListOfPlayers.append(id)
 	var NewPlayer = load("res://Player/Player.tscn").instantiate()
 	add_child(NewPlayer)
 	NewPlayer.position = get_viewport().size / 2
+	if ServerStarted:
+		MenuLabel.text = "Player " + str(CurrentPlayerIndex) + " disconnected"
+	
 
 func _on_peer_disconnected(id):
-	print("⚠️ Peer disconnected: %s" % id)
+	print("Peer disconnected: %s" % id)
+	var indexOfDisconnectedPlayer = ListOfPlayers.find(id)
+	ListOfPlayers.remove_at(indexOfDisconnectedPlayer)
+	print(get_child(indexOfDisconnectedPlayer))
+	remove_child(get_child(indexOfDisconnectedPlayer))
+	if ServerStarted:
+		MenuLabel.text = "Player " + str(indexOfDisconnectedPlayer) + " disconnected"
 
 func getPlayerOffID(id):
 	return get_child(ListOfPlayers.find(id))	
 
 @rpc("any_peer")
 func sentPlayerColor(sentColor):
-	sentColor = Color(randf(), randf(), randf())
 	var player = getPlayerOffID(multiplayer.get_remote_sender_id())
-	player.get_child(0).texture.gradient.set_color(0, sentColor)
-	player.get_child(0).get_child(0).default_color = sentColor
+	var mesh = player.get_child(0)
+	var line = mesh.get_child(0)
+	
+	var new_gradient = mesh.texture.gradient.duplicate()
+	mesh.texture.gradient = new_gradient
+	new_gradient.set_color(0, sentColor)
+
+	var mew_gradient = line.gradient.duplicate()
+	line.gradient = new_gradient
+	new_gradient.set_color(0, sentColor)
+
+	print(ListOfPlayers.find(multiplayer.get_remote_sender_id()))
+
+@rpc("any_peer")
+func getInfo(_information):
+	pass
 
 @rpc("any_peer")
 func send_gyro(GyroData):
 	
-	var DPI = (get_viewport().size.x + get_viewport().size.y) / 160
+	var moveDPI = DPI / 140
 	var player = getPlayerOffID(multiplayer.get_remote_sender_id())
+	
+	player.rotation += GyroData.y
+	if player.rotation >= 360 or player.rotation <= -360:
+		player.rotation = 0
+	print(player.rotation)
 	
 	if player.position.x >= get_viewport().size.x:
 		player.position.x = 0
@@ -64,9 +91,21 @@ func send_gyro(GyroData):
 		player.position.y = get_viewport().size.y
 	
 	
-	player.position.x += GyroData.z * -DPI
-	player.position.y += GyroData.x * -DPI 
+	player.position.x += GyroData.z * -moveDPI
+	player.position.y += GyroData.x * -moveDPI 
 
 @rpc("any_peer")
 func send_print():
-	print("Recieved a signal")
+	print("Recieved a signal, meaning proper connection was established!")
+	getInfo.rpc_id(multiplayer.get_remote_sender_id(), "Established connection!")
+
+
+func _on_resized() -> void:
+	DPI = (get_viewport().size.x + get_viewport().size.y)
+	if ServerStarted:
+		MenuLabel.text = "Game resized, changed DPI to " + str(DPI)
+
+
+func _on_start_game_body_entered(body: Node2D) -> void:
+	print(body)
+	MenuLabel.text = "Body " + str(body) + " entered start game area!"
--- Player/trailLine.gd
+++ Player/trailLine.gd
@@ -2,5 +2,5 @@ extends Line2D
 
 func _process(delta: float) -> void:
 	add_point(get_parent().global_position)
-	if points.size() > 30:
+	if points.size() > 50:
 		remove_point(0)
